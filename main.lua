1891a333ac80cd8a25752d19ec55df68d7f8ff5c
b27xip <85450257+b27xip@users.noreply.github.com>
2021-06-11 15:45:06 +0200

    Delete main.lua

--- main.lua
+++
@@ -1,6 +0,0 @@
-local spamsettings = ({
-	text = "im like spaming",
-	waitingtime = 1,
-	settingstogui = true,
-});
-loadstring(game:HttpGet('https://raw.githubusercontent.com/b27xip/spamscript/main/spam.lua'))();
