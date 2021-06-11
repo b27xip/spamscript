710ee122fea1b444f06121c2e950199b9f580b7a
b27xip <85450257+b27xip@users.noreply.github.com>
2021-06-11 15:44:12 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+local spamsettings = ({
+	text = "im like spaming",
+	waitingtime = 1,
+	settingstogui = true,
+});
+loadstring(game:HttpGet('https://raw.githubusercontent.com/b27xip/spamscript/main/spam.lua'))();
